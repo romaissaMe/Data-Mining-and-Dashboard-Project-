999eaaa0c209b94c692b479d36084bcc310f34f9
unknown <roumamedj@gmail.com>
2023-12-26 20:38:38 +0100

    first commit

---
+++ algorithms.py
@@ -0,0 +1,171 @@
+from collections import Counter
+import numpy as np
+class Node:
+    def __init__(self, feature=None, threshold=None, left=None, right=None, *, value=None):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.value = value
+    def is_leaf_node(self):
+        return self.value is not None
+
+class DecisionTree:
+    def __init__(self, min_samples_split=2, max_depth=100, n_feats=None):
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+        self.n_feats = n_feats  # no. of features to consider
+        self.root = None
+
+    def fit(self, X, y):
+        self.n_feats = X.shape[1] if not self.n_feats else min(self.n_feats, X.shape[1])
+        self.root = self._grow_tree(X, y)
+
+    def _grow_tree(self, X, y, depth=0):
+        n_samples, n_features = X.shape
+        n_labels = len(np.unique(y))
+
+        # stopping criteria
+        if (depth >= self.max_depth
+            or n_labels == 1
+            or n_samples < self.min_samples_split):
+            leaf_value = self._most_common_label(y)
+            return Node(value=leaf_value)
+
+        feat_idxs = np.random.choice(n_features, self.n_feats, replace=False)
+
+        # greedily select the best split according to information gain
+        best_thresh, best_feature = self._best_split(X, y, feat_idxs)
+        #create child nodes
+        left_idxs, right_idxs = self._split(X[:, best_feature], best_thresh)
+        # grow the children that result from the split
+        left_child = self._grow_tree(X[left_idxs,:], y[left_idxs], depth=depth+1)
+        right_child = self._grow_tree(X[right_idxs,:], y[right_idxs], depth=depth+1)
+        return Node(best_feature, best_thresh, left_child, right_child)
+
+    def _best_split(self, X, y, feat_idxs):
+        best_gain = -1
+        split_idx, split_thresh = None, None
+        for feat_idx in feat_idxs:
+            X_column = X[:, feat_idx]
+            thresholds = np.unique(X_column)
+            for threshold in thresholds:
+                # calculate the information gain
+                gain = self._information_gain(y, X_column, threshold)
+
+                if gain > best_gain:
+                    best_gain = gain
+                    split_idx = feat_idx
+                    split_thresh = threshold
+
+        return split_thresh, split_idx
+    def _most_common_label(self, y):
+        counter = Counter(y)
+        most_common = counter.most_common(1)[0][0]
+        return most_common
+    
+    def _information_gain(self, y, X_column, threshold):
+        # parent entropy
+        parent_entropy = self._entropy(y)
+
+        # generate split
+        left_idxs, right_idxs = self._split(X_column, threshold)
+
+        if len(left_idxs) == 0 or len(right_idxs) == 0:
+            return 0
+
+        # calculate the weighted avg. of the loss for the children
+        n = len(y)
+        n_l, n_r = len(left_idxs), len(right_idxs)
+        e_l, e_r = self._entropy(y[left_idxs]), self._entropy(y[right_idxs])
+        child_entropy = (n_l / n) * e_l + (n_r / n) * e_r
+
+        # calculate the IG
+        information_gain = parent_entropy - child_entropy
+        return information_gain
+    
+    def _entropy(self, y):
+        hist = np.bincount(y)
+        ps = hist / len(y)
+        return -np.sum([p * np.log(p) for p in ps if p > 0])
+    
+    def _split(self, X_column, split_thresh):
+        left_idxs = np.argwhere(X_column <= split_thresh).flatten()
+        right_idxs = np.argwhere(X_column > split_thresh).flatten()
+        return left_idxs, right_idxs
+    
+    def predict(self, X):
+        return np.array([self._traverse_tree(x, self.root) for x in X])
+
+    def _traverse_tree(self, x, node):
+        if node.is_leaf_node():
+            return node.value
+
+        if x[node.feature] <= node.threshold:
+            return self._traverse_tree(x, node.left)
+        return self._traverse_tree(x, node.right)
+    
+
+################################################# RANDOM FORESTS ##############################################
+class RandomForest:
+    def __init__(self, n_trees=10, max_depth=10, min_samples_split=2, n_feature=None):
+        self.n_trees = n_trees
+        self.max_depth=max_depth
+        self.min_samples_split=min_samples_split
+        self.n_features=n_feature
+        self.trees = []
+
+    def fit(self, X, y):
+        self.trees = []
+        for _ in range(self.n_trees):
+            tree = DecisionTree(max_depth=self.max_depth,
+                            min_samples_split=self.min_samples_split,
+                            n_feats=self.n_features)
+            X_sample, y_sample = self._bootstrap_samples(X, y)
+            tree.fit(X_sample, y_sample)
+            self.trees.append(tree)
+
+    def _bootstrap_samples(self, X, y):
+        n_samples = X.shape[0]
+        idxs = np.random.choice(n_samples, n_samples, replace=True)
+        return X[idxs], y[idxs]
+
+    def _most_common_label(self, y):
+        counter = Counter(y)
+        most_common = counter.most_common(1)[0][0]
+        return most_common
+
+    def predict(self, X):
+        predictions = np.array([tree.predict(X) for tree in self.trees])
+        tree_preds = np.swapaxes(predictions, 0, 1)
+        predictions = np.array([self._most_common_label(pred) for pred in tree_preds])
+        return predictions
+    
+############################################### KNN ##############################################
+def euclidean_distance(x1, x2):
+    distance = np.sqrt(np.sum((x1-x2)**2))
+    return distance
+
+class KNN:
+    def __init__(self, k=3):
+        self.k = k
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+    def predict(self, X):
+        predictions = [self._predict(x) for x in X]
+        return predictions
+
+    def _predict(self, x):
+        # compute the distance
+        distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
+    
+        # get the closest k
+        k_indices = np.argsort(distances)[:self.k]
+        k_nearest_labels = [self.y_train[i] for i in k_indices]
+
+        # majority voye
+        most_common = Counter(k_nearest_labels).most_common()
+        return most_common[0][0]
